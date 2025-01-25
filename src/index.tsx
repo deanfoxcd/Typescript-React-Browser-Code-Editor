c08709a532cc14972659f1ec4a3d0218ce88bb85
Dean Fox <deanfoxcd@gmail.com>
2025-01-24 20:00:57 -0500

    add transpiling

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import * as esbuild from 'esbuild-wasm';
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  function handleClick() {}
+  const startService = async function () {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  async function handleClick() {
+    if (!ref.current) return;
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  }
 
   return (
     <div>
