a9b38a07f6b8f5289fcaa1d7a824dd1acef6ecb8
Dean Fox <deanfoxcd@gmail.com>
2025-02-04 21:33:35 -0500

    add bundles reducer

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,5 +1,7 @@
+import { Dispatch } from 'react';
 import { ActionType } from '../action-types';
 import {
+  Action,
   DeleteCellAction,
   Direction,
   InsertCellAfterAction,
@@ -7,6 +9,7 @@ import {
   UpdateCellAction,
 } from '../actions';
 import { CellTypes } from '../cell';
+import bundle from '../../bundler';
 
 export function updateCell(id: string, content: string): UpdateCellAction {
   return {
@@ -45,5 +48,29 @@ export function insertCellAfter(
       id,
       type: cellType,
     },
+  };
+}
+
+export function createBundle(cellId: string, input: string) {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId,
+      },
+    });
+
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: {
+          code: result.code,
+          error: result.err,
+        },
+      },
+    });
   };
 }
